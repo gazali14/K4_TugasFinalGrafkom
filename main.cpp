f4f1952a786bfd7123dc9883a0056016c7f8c69c
Laode.jafar <jafarode99@gmail.com>
2021-12-25 12:24:09 +0800

    commit Jafar

--- main.cpp
+++ main.cpp
@@ -47,3 +47,53 @@ void display(void)
  */
 
   glTranslatef(0,0,-100);// besar kecilnya mobil
+ Loop++;
+     glRotatef(Loop,0.9,4.0,0.6);//mobil yang berotasi
+
+ glPushMatrix();//body
+ //glpushmatrix();
+ glColor3f(0.0,0.0,9.0);
+ blok(10, 3, 2);
+
+ glTranslatef(0, 9, 0);
+ blok(10, 3, 2);
+ glTranslatef(10, -10,0);
+ blok(10, 5.5, 2);
+ glRotatef(-35, 0, 0, 15);
+ glTranslatef(0, 7,0);
+ blok(10, 2, 2);
+ glTranslatef(2, 4.9,-2.5);
+ glColor3f(0.9,0.9,0.9);// warna kaca
+ blok(0.5, 20, 31);
+
+ glRotatef(180, 45, -45, 0);
+ //glTranslatef(0, 10,0);
+ //blok(3, 2, 5);
+ //cylinder(2,2,30);
+ //glTranslatef(-10, -10,0);
+ //glRotatef(90, 45,-45, 0);
+ //cylinder(2,2,30);
+ //glRotatef(90, -45, 90, 90);
+ //blok(5, 3, 2);
+
+ glPopMatrix();
+
+
+ glPushMatrix();//roda
+ glColor3f(0.0,0.0,0.0);
+ glTranslatef(20, -8,-7);
+ cylinder(5, 5,3);
+
+ glColor3f(0.0,0.0,0.0);
+ glTranslatef(-20, 8,7);
+ glTranslatef(-5, -8,-7);
+ cylinder(5, 5,3);
+
+ glColor3f(0.0,0.0,0.0);
+ glTranslatef(5, 8,7);
+ glRotatef(180,0,180,0);
+ glTranslatef(3,-8,-17);
+ cylinder(5, 5,3);
+
+ glColor3f(0.0,0.0,0.0);
+ glTranslatef(-3,8,17);
